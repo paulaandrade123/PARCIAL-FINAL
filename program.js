6ccfa3e18e4433d67cb165c1c12a1b2c6748a1a4
Paula andrade <paulaandz22@gmail.com>
2025-03-06 20:31:29 -0500

    v0.3

--- program.js
+++ program.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var map1 = L.map1("map1").setView([4.6985, -74.1173], 15);
+    var map = L.map("map").setView([4.6985, -74.1173], 15);
     var trees = []; // Guardará los árboles cargados
 
     // Capa base de OpenStreetMap
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: '© OpenStreetMap contributors'
-    }).addTo(map1);
+    }).addTo(map);
 
     // Polígono de Álamos Norte
     var alamosNortePolygon = [
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
         weight: 2,
         fillColor: "rgba(255, 0, 0, 0.3)",
         fillOpacity: 0.4
-    }).addTo(map1).bindPopup("🗺️ Barrio Álamos Norte");
+    }).addTo(map).bindPopup("🗺️ Barrio Álamos Norte");
 
    
 
